1e021d2504baeae5dd9d20cd4ded5e9463aa6424
Daniel Lütgehetmann <luetgehetmann@googlemail.com>
2016-05-31 22:25:38 +0200

    Magicline images

--- dev/builder/buildDist.sh
+++ dev/builder/buildDist.sh
@@ -12,11 +12,11 @@ RELEASE_DIR=$BASE_DIR/release/ckeditor
 mkdir -p $TARGET_DIR
 rm -rf $TARGET_DIR/*
 
-FILES=( "/ckeditor.js" "/styles.js" "/contents.css" "/skins/mathhire/skin.js" "/skins/mathhire/dialog.css" "/skins/mathhire/editor.css" "/lang/en.js" )
+FILES=( "/ckeditor.js" "/styles.js" "/contents.css" "/skins/mathhire/skin.js" "/skins/mathhire/dialog.css" "/skins/mathhire/editor.css" "/lang/en.js" "/plugins/magicline/images" )
 for FILE in "${FILES[@]}"
 do
     mkdir -p `dirname $TARGET_DIR$FILE`
-    cp $RELEASE_DIR$FILE $TARGET_DIR$FILE
+    cp -R $RELEASE_DIR$FILE $TARGET_DIR$FILE
 done
 
 cp $BASE_DIR/dist-config.js $TARGET_DIR/config.js
